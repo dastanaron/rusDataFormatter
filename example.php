bf0187a46da358710057cf7c47be66632c089fa2
Дмитрий <flow199@yandex.ru>
2018-02-22 10:24:37 +0300

    Update example.php

--- example.php
+++ example.php
@@ -5,7 +5,7 @@ use dastanaron\extension\RuDate;
 
 $date = RuDate::init('2018-10-18 19:43');
 
-var_dump($date->format('l, d F Y M D')); // string(50) "Вторник, 20 Февраля 2018 Фев Вт"
+var_dump($date->format('l, d F Y M D'));
 
 
 
